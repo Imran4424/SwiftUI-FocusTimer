514c7735f9b58d52449bf665e5c904dfaeb238d8
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-06 12:42:00 +0600

    project init

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Shah Md Imran Hossain on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
