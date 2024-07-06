a9f6ec42901b3338da01d753f4b3a164bcdb562c
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-06 19:21:30 +0600

    scene phase

--- FocusTimer/FocusTimerApp.swift
+++ FocusTimer/FocusTimerApp.swift
@@ -12,11 +12,37 @@ struct FocusTimerApp: App {
     // MARK: - Since, we are doing background fetching. Initializing here
     @StateObject var focusModel: FocusModel = .init()
     
+    // MARK: - Scene phase
+    @Environment(\.scenePhase) var phase
+    
+    // MARK: - storing last time stamp
+    @State var lastActiveTimeStamp: Date = Date()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(focusModel)
                 .preferredColorScheme(.dark)
+        }
+        .onChange(of: phase) { oldValue, newValue in
+            if focusModel.isStarted {
+                if newValue == .background {
+                    lastActiveTimeStamp = Date()
+                }
+                
+                if newValue == .active {
+                    // MARK: - finding the difference
+                    let currentTimeStampDiff = Date().timeIntervalSince(lastActiveTimeStamp)
+                    
+                    if focusModel.totalSeconds - Int(currentTimeStampDiff) <= 0 {
+                        focusModel.isStarted = false
+                        focusModel.totalSeconds = 0
+                        focusModel.isFinished = true
+                    } else {
+                        focusModel.totalSeconds -= Int(currentTimeStampDiff)
+                    }
+                }
+            }
         }
     }
 }
