a4a5374263a56545a232ea85fae4834a6899e361
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-06 16:40:58 +0600

    design home view

--- FocusTimer/Model/FocusModel.swift
+++ FocusTimer/Model/FocusModel.swift
@@ -9,5 +9,12 @@ import SwiftUI
 
 class FocusModel: NSObject, ObservableObject {
     // MARK: - Timer Properties
+    @Published var progress: CGFloat = 1
+    @Published var timerStringValue: String = "00:00"
+    @Published var isStarted: Bool = false
+    @Published var addNewTimer: Bool = false
     
+    @Published var hours: Int = 0
+    @Published var minutes: Int = 0
+    @Published var seconds: Int = 0
 }
--- FocusTimer/View/Home.swift
+++ FocusTimer/View/Home.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Home: View {
+    @EnvironmentObject private var focusModel: FocusModel
+    
     var body: some View {
         VStack {
             Text("Focus Timer")
@@ -23,12 +25,12 @@ struct Home: View {
                             .padding(-40)
                         
                         Circle()
-                            .trim(from: 0, to: 0.5)
+                            .trim(from: 0, to: focusModel.progress)
                             .stroke(.white.opacity(0.03), lineWidth: 80)
                         
                         // MARK: - Shadow
                         Circle()
-                            .stroke(Color("Purple"), lineWidth: 5)
+                            .stroke(Color("mPurple"), lineWidth: 5)
                             .blur(radius: 15)
                             .padding(-2)
                         
@@ -36,15 +38,15 @@ struct Home: View {
                             .fill(Color("BG"))
                         
                         Circle()
-                            .trim(from: 0, to: 0.5)
-                            .stroke(Color("Purple").opacity(0.7), lineWidth: 10)
+                            .trim(from: 0, to: focusModel.progress)
+                            .stroke(Color("mPurple").opacity(0.7), lineWidth: 10)
                         
                         // MARK: - Knob
                         GeometryReader { proxy in
                             let size = proxy.size
                             
                             Circle()
-                                .fill(Color("Purple"))
+                                .fill(Color("mPurple"))
                                 .frame(width: 30, height: 30)
                                 .overlay {
                                     Circle()
@@ -54,12 +56,40 @@ struct Home: View {
                                 .frame(width: size.width, height: size.height, alignment: .center)
                                 // since view is rotated, that's why using x
                                 .offset(x: size.height / 2)
+                                .rotationEffect(.init(degrees: focusModel.progress * 360))
                             
                         }
+                        
+                        Text(focusModel.timerStringValue)
+                            .font(.system(size: 45, weight: .light))
+                            .rotationEffect(.init(degrees: -90))
+                            .animation(.none, value: focusModel.progress)
                     }
                     .padding(60)
                     .frame(height: proxy.size.width)
                     .rotationEffect(.init(degrees: -90))
+                    .animation(.easeOut, value: focusModel.progress)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    
+                    Button {
+                        if focusModel.isStarted {
+                            
+                        } else {
+                            focusModel.addNewTimer = true
+                        }
+                        
+                    } label: {
+                        Image(systemName: focusModel.isStarted ? "pause" : "timer")
+                            .font(.largeTitle.bold())
+                            .foregroundStyle(Color.white)
+                            .frame(width: 80, height: 80)
+                            .background {
+                                Circle()
+                                    .fill(Color("DarkPurple"))
+                            }
+                            .shadow(color: Color("mPurple"), radius: 8, x: 0, y: 0)
+                    }
+
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
