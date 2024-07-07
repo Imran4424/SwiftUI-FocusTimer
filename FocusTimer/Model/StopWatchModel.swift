b4d9fb3fc1d9bbd5ffa6bbd226b2a961c709fa2e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-07 23:44:39 +0600

    design stop watch UI

--- FocusTimer/Model/StopWatchModel.swift
+++ FocusTimer/Model/StopWatchModel.swift
@@ -8,5 +8,14 @@
 import SwiftUI
 
 class StopWatchModel: NSObject, ObservableObject {
+    // MARK: - Stopwatch properties
+    @Published var watchStringValue = "00:00:00"
+    @Published var isStarted: Bool = false
     
+    @Published var hours: Int = 0
+    @Published var minutes: Int = 0
+    @Published var seconds: Int = 0
+    
+    // MARK: - total seconds
+    @Published var totalSeconds: Int = 0
 }
--- FocusTimer/View/StopWatchView.swift
+++ FocusTimer/View/StopWatchView.swift
@@ -19,7 +19,62 @@ struct StopWatchView: View {
             GeometryReader { proxy in
                 VStack(spacing: 15) {
                     // MARK: - Timer Ring
+                    ZStack {
+                        Circle()
+                            .fill(Color("BG"))
+                            .padding(-40)
+                        
+                        Circle()
+                            .stroke(Color("mPurple").opacity(0.7), lineWidth: 15)
+                            .padding(-40)
+                        
+                        // MARK: - Shadow
+                        Circle()
+                            .stroke(Color("mPurple"), lineWidth: 5)
+                            .blur(radius: 15)
+                            .padding(-15)
+                        
+                        Text(stopWatchModel.watchStringValue)
+                            .font(.system(size: 45, weight: .light))
+//                            .rotationEffect(.init(degrees: 90))
+//                            .animation(.none, value: focusModel.progress)
+                    }
+                    .padding(60)
+                    .frame(height: proxy.size.width)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                     
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "clock.arrow.circlepath")
+                                .font(.largeTitle.bold())
+                                .foregroundStyle(Color.white)
+                                .frame(width: 80, height: 80)
+                                .background {
+                                    Circle()
+                                        .fill(Color("DarkPurple"))
+                                }
+                                .shadow(color: Color("mPurple"), radius: 8, x: 0, y: 0)
+                        }
+                        
+                        Spacer()
+
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: stopWatchModel.isStarted ?  "pause" : "play")
+                                .font(.largeTitle.bold())
+                                .foregroundStyle(Color.white)
+                                .frame(width: 80, height: 80)
+                                .background {
+                                    Circle()
+                                        .fill(Color("DarkPurple"))
+                                }
+                                .shadow(color: Color("mPurple"), radius: 8, x: 0, y: 0)
+                        }
+                    }
+                    .padding(.horizontal)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
